b762ec0d9c47e5fb28382eb0a6fb614781029c17
KormuhinIP <zero14244@Yandex.ru>
2018-12-12 12:14:55 +0400

    изменил на IOUtils.readFully()

--- src/main/java/org/tlauncher/log/inspector/service/FileService.java
+++ src/main/java/org/tlauncher/log/inspector/service/FileService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.tlauncher.log.inspector.model.ClientType;
+import sun.misc.IOUtils;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -34,16 +35,16 @@ public class FileService  {
 
     public synchronized void processLog(String io, ClientType version, MultipartFile file) throws Exception{   //Обрабока входных данных и сохранение
             count.getAndIncrement();
+
             ZipInputStream zip = new ZipInputStream(file.getInputStream());
             File filePath = new File(version+"/"+io+"/"+olddate);
             filePath.mkdirs();
             File file2 = new File(filePath +"/"+String.valueOf(count)+"_"+ zip.getNextEntry().getName());
             FileOutputStream bos = new FileOutputStream(file2);
-            byte[] buffer = new byte[64000];
-            int location;
-            while ((location = zip.read(buffer)) != -1) {
-                bos.write(buffer, 0, location);
-            }
+            byte[] buffer;
+            buffer=IOUtils.readFully(zip,-1, true);
+            bos.write(buffer, 0, buffer.length);
+
            bos.close();
            zip.close();
     }
